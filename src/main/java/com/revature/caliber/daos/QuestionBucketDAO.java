8fb639232cbeddf7afd41f4639449a2256404970
adil iqbal <iqad009@gmail.com>
2018-07-20 10:10:15 -0400

    first skeleton

---
+++ src/main/java/com/revature/caliber/daos/QuestionBucketDAO.java
@@ -0,0 +1,34 @@
+package com.revature.caliber.daos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.revature.caliber.beans.QuestionBucket;
+
+
+@Repository
+public interface QuestionBucketDAO extends JpaRepository<QuestionBucket, Integer>{
+	
+	@Query("insert into BUCKET (bucket_id, bucket_category, bucket_description, is_active, skill_type_id) values (?,?,?,?,?)")
+	public int createBucket(QuestionBucket questionbucket);
+	
+	@Query("select * from BUCKET")
+	public List<QuestionBucket> getAllBuckets();
+	
+	//byid
+	@Query("select * from BUCKET where bucket_id = ?")
+	public QuestionBucket getBucketById(Integer bucketId);
+	
+	//bycategory
+	@Query("select * from BUCKET where bucket_category = ?")
+	public QuestionBucket getBucketByCategory(Integer bucketCategory);
+	
+	//update
+	
+	//delete
+	
+
+}
---
+++ src/main/java/com/revature/caliber/services/BucketService.java
@@ -0,0 +1,59 @@
+package com.revature.caliber.services;
+import java.util.List;
+import com.revature.caliber.beans.QuestionBucket;
+
+
+/**
+ * Interface for our Bucket service layer
+ *  
+ * @author Angelique Elkins | 1803-USF-MAR26 | Wezley Singleton 
+ * @author Philip Escobedo | 1803-USF-MAR26 | Wezley Singleton
+ * @author Brandon Semba | 1803-USF-MAR26 | Wezley Singleton
+ */
+public interface BucketService {
+
+	/**
+	 * Saves given Bucket to database
+	 * 
+	 * @author Angelique Elkins | 1803-USF-MAR26 | Wezley Singleton 
+	 * @author Philip Escobedo | 1803-USF-MAR26 | Wezley Singleton
+	 * 
+	 * @param bucket - Bucket to save
+	 * @return saved Bucket
+	 */
+	public QuestionBucket save(QuestionBucket bucket);
+
+	/**
+	 * Returns all Buckets
+	 * 
+	 * @author Brandon Semba | 1803-USF-MAR26 | Wezley Singleton
+	 * 
+	 * @return all Buckets
+	 */
+	public List<QuestionBucket> getAllBuckets();
+
+	/**
+	 * Returns Bucket of given id
+	 * 
+	 * @author Brandon Semba | 1803-USF-MAR26 | Wezley Singleton
+	 * 
+	 * @param bucketId - the id of the Bucket being located
+	 * @return the Bucket whose id was matched; return null if not found
+	 */
+	public QuestionBucket getBucketById(Integer bucketId);
+
+	/**
+	 * Updates given Bucket to database
+	 * 
+	 * @author Angelique Elkins | 1803-USF-MAR26 | Wezley Singleton
+	 * @author Philip Escobedo | 1803-USF-MAR26 | Wezley Singleton
+	 * 
+	 * @param bucket - updated Bucket
+	 * @return updated Bucket
+	 */
+	public QuestionBucket getBucketByCategory(Integer bucketCategory);
+	
+	public QuestionBucket createBucket(QuestionBucket questionbucket);
+	public QuestionBucket updateBucket(QuestionBucket bucket);
+}
+	
