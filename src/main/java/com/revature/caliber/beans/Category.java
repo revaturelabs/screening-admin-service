69af65fd035c9c572576eb12a0ab1e062b98f2e1
ethanco81 <ethanco81@ymail.com>
2018-07-20 14:11:20 -0400

    SQL file for h2 and added isActive boolean to Category POJO

--- src/main/java/com/revature/caliber/beans/Category.java
+++ src/main/java/com/revature/caliber/beans/Category.java
@@ -32,70 +32,51 @@ public class Category implements Serializable{
     @ApiModelProperty(value = "name of the Categories title")
     @Column(name = "Title")
         private String title;
+    
+    @ApiModelProperty(value = "is the category currently active")
+    @Column(name = "is_active")
+    private boolean isActive;
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int categoryId, String title, boolean isActive) {
+		super();
+		this.categoryId = categoryId;
+		this.title = title;
+		this.isActive = isActive;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
         
-        /**
-         * Instantiates a new Category.
-         */
-        
-        
-        public Category() {
-            super();
-        }
-
-        public Category(int categoryId, String title) {
-            super();
-            this.categoryId = categoryId;
-            this.title = title;
-        }
-
-        public int getCategoryId() {
-            return categoryId;
-        }
-
-        public void setCategoryId(int categoryId) {
-            this.categoryId = categoryId;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + categoryId;
-            result = prime * result + ((title == null) ? 0 : title.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Category other = (Category) obj;
-            if (categoryId != other.categoryId)
-                return false;
-            if (title == null) {
-                if (other.title != null)
-                    return false;
-            } else if (!title.equals(other.title))
-                return false;
-            return true;
-        }
-        
-        @Override
-        public String toString() {
-            return "Category [categoryId=" + categoryId + ", title=" + title + "]";
-        }
+    
 
 }
