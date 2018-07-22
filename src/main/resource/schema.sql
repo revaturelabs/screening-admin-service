4da9ad3f92808b4ab0044503959d8cd3f64ddd70
ethanco81 <ethanco81@ymail.com>
2018-07-21 20:03:15 -0400

    Working H2 DB

--- src/main/resource/schema.sql
+++ src/main/resource/schema.sql
@@ -8,7 +8,7 @@ create table question(
     sample_answer_3 varchar2(500) not null,
     sample_answer_4 varchar2(500) not null,
     sample_answer_5 varchar2(500) not null
-)
+);
 
 create table bucket(
     bucket_id number(10) primary key,
@@ -16,21 +16,32 @@ create table bucket(
     bucket_description varchar2(100) not null,
     is_active number(1) not null,
     skill_type_id number(10) not null
-)
+);
 
 create table skill_type(
     skill_type_id number(10) primary key,
     title varchar2(100) not null
-)
+);
 
 create table catigory(
     category_id number(10) primary key,
-    title varchar2(100) not null
-)
+    title varchar2(100) not null,
+    is_active number(1) not null
+);
 
 create table weight(
     weight_id number(10) primary key,
     weight number(10) not null,
     category_id number(10) not null,
     skill_type_id number(10) not null
-)
+);
+
+CREATE SEQUENCE IF NOT EXISTS QUESTION_SEQUENCE MINVALUE 100001 INCREMENT BY 1 START WITH 100001;
+
+CREATE SEQUENCE IF NOT EXISTS BUCKET_SEQUENCE MINVALUE 100001 INCREMENT BY 1 START WITH 100001;
+
+CREATE SEQUENCE IF NOT EXISTS SKILL_TYPE_SEQUENCE MINVALUE 100001 INCREMENT BY 1 START WITH 100001;
+
+CREATE SEQUENCE IF NOT EXISTS CATEGORY_SEQUENCE MINVALUE  100001 INCREMENT BY 1 START WITH  100001;
+
+CREATE SEQUENCE IF NOT EXISTS WEIGHT_SEQUENCE MINVALUE  100001 INCREMENT BY 1 START WITH  100001;
