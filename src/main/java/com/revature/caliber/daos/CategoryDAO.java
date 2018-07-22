72573831aca6591201300b7a481319d260b7c301
zia mohiuddin <zain-sheikh@live.com>
2018-07-22 15:25:16 -0400

    ViewFeature

---
+++ src/main/java/com/revature/caliber/daos/CategoryDAO.java
@@ -0,0 +1,29 @@
+package com.revature.caliber.daos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.revature.caliber.beans.Category;
+
+@Repository
+public interface CategoryDAO extends JpaRepository<Category, Integer> {
+	
+  //  @Query("select * from Category where Category_id = :category_id")
+//	public Category findByCatergoryID(int id);
+//	@Query("insert into Category (category_id, category_title, is_active) values (?,?,?)")
+//	public Category create(Category category);
+	
+	//@Query("select * from Category")
+	//@Query("select c from Category c where c.Category_id = ?1")
+	//@Query("select c from Category c where c.Category_id IN (?1)")
+	//@Query("select c from Category c where c.Category_id = :Category_id")
+	public List<Category> findAll();
+	
+//	public Category saveCategory(Category category);
+//	public void deleteByCategoryID(int id);
+//	public List<Category> findAllActive();
+	
+}
---
+++ src/main/java/com/revature/caliber/services/CategoryService.java
@@ -0,0 +1,53 @@
+package com.revature.caliber.services;
+
+import java.util.List;
+
+import com.revature.caliber.beans.Category;
+
+public interface CategoryService {
+	/**
+	 * Adds a new Category to the database
+	 * 
+	 * @param Category - Category to be added
+	 * @return a Category that has been added
+	 */
+//	Category create(Category category);
+
+	/**
+	 * Retrieves all the Categories, active and non-active
+	 * 
+	 * @return list containing all the Categories found
+	 */
+	List<Category> findAll();
+
+	/**
+	 * Retrieves all the active Categories
+	 * 
+	 * @return list containing all the Categories found
+	 */
+//	List<Category> findAllActive();
+
+	/**
+	 * Retrieves a Category by id
+	 * 
+	 * @param id - id of Category being retrieved
+	 * @return a Category
+	 */
+//	Category findByCategoryID(int id);
+
+	/**
+	 * Adds a new Category to the DB, but unlike create(), this method will
+	 * commit/flush changes to DB immediately
+	 * 
+	 * @param Category - Category to be added
+	 * @return a Category that was added
+	 */
+//	Category saveCategory(Category category);
+
+	/**
+	 * Deletes a Category by id
+	 * 
+	 * @param id - id of the Category to delete
+	 */
+//	void deleteByCategoryID(int id);
+}
