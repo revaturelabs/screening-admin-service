67d40aee02710bd59f3ff2f51fd4652850a6c442
zia mohiuddin <zain-sheikh@live.com>
2018-07-23 15:14:50 -0400

    deleted the resource folder and unused imports

--- src/main/java/com/revature/caliber/daos/CategoryDAO.java
+++ src/main/java/com/revature/caliber/daos/CategoryDAO.java
@@ -3,7 +3,6 @@ package com.revature.caliber.daos;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.caliber.beans.Category;
@@ -14,7 +13,5 @@ public interface CategoryDAO extends JpaRepository<Category, Integer> {
 	@Query("select * from Category")
 */	
 	public List<Category> findAll();
-	
-
-	
+		
 }
--- src/main/java/com/revature/caliber/services/CategoryServiceImpl.java
+++ src/main/java/com/revature/caliber/services/CategoryServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.revature.caliber.beans.Category;
 import com.revature.caliber.daos.CategoryDAO;
 
