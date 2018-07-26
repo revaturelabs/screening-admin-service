62c5ce83109259fb6b15317383fb5c4c7f64bb55
Ethan Conner <EthanCo81@ymail.com>
2018-07-26 19:01:05 -0400

    Update QuestionController.java

--- src/main/java/com/revature/caliber/controllers/QuestionController.java
+++ src/main/java/com/revature/caliber/controllers/QuestionController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -35,7 +34,6 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/questions")
 @CrossOrigin
-@ComponentScan("com.revature.caliber.*")
 public class QuestionController {
 	@Autowired
 	private QuestionService qs;
