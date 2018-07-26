a5a25efb109119097d96a940853f85679ad62f59
ethanco81 <ethanco81@ymail.com>
2018-07-26 11:08:49 -0400

    compiles, still need to figure out how it works

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -2,7 +2,12 @@ package com.revature.caliber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
