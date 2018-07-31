7971be6a8b3157513023b6abbbd8b7caeb486b0b
Ethan Conner <EthanCo81@ymail.com>
2018-07-31 15:18:10 -0400

    Update BucketServiceImp.java

--- src/main/java/com/revature/caliber/services/BucketServiceImp.java
+++ src/main/java/com/revature/caliber/services/BucketServiceImp.java
@@ -8,17 +8,21 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import com.revature.caliber.beans.Bucket;
 import com.revature.caliber.daos.BucketDAO;
 
 
+/**
+ * Implementation for our Bucket service layer
+ *  
+ *  @author adil iqbal 		| 1805-WVU-MAY29 | Richard Orr
+ *  @author Theo Thompson 	| 1805-WVU-MAY29 | Richard Orr
+ */
 @Service
 public class BucketServiceImp implements BucketService {
-	
+
 	@Autowired
-	private BucketDAO bucketDAO;
-	
+	BucketDAO bucketDAO;
 	
 	@Transactional
 	@Override
@@ -31,15 +35,16 @@ public class BucketServiceImp implements BucketService {
 		List<Bucket> buckets = new ArrayList<>();
 		bucketDAO.findAll().forEach(buckets::add);
 		return buckets;
-	}
-	
+	}	
 
 	@Override
+	@Transactional
 	public void updateBucket(Bucket bucket) {
 		bucketDAO.save(bucket);
 	}
 	
 	@Override
+	@Transactional
 	public void deleteBucket(Integer bucketId) {
 		bucketDAO.delete(bucketId);
 	}
@@ -48,4 +53,5 @@ public class BucketServiceImp implements BucketService {
 	public Bucket getBucketById(Integer bucketId) {
 		return bucketDAO.findOne(bucketId);
 	}
+	
 }
