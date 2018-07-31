c953a41e39f4e2565aabb096fd112e4789989800
Ethan Conner <EthanCo81@ymail.com>
2018-07-31 15:41:16 -0400

    Update SkillTypeController.java

--- src/main/java/com/revature/caliber/controllers/SkillTypeController.java
+++ src/main/java/com/revature/caliber/controllers/SkillTypeController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,23 +27,20 @@ import io.swagger.annotations.ApiResponses;
  * @author Isaac Pawling | 1805-WVU | Richard Orr
  */
 @RestController
-@RequestMapping(value = "/skillType")
-@CrossOrigin
-@ApiModel(value = "SkillTypeController", description = "A rest controller to handle HTTP Requests made to /skillTypes")
+@RequestMapping(value = "/skilltype")
+@ApiModel(value = "SkillTypeController", description = "A rest controller to handle HTTP Requests made to /skilltype")
 public class SkillTypeController {
 
 	@Autowired
 	SkillTypeService skillService;
 	
 	/**
-	 * Request that returns all skill types
+	 * Gets all skill types
 	 * 
 	 * @return List of all SkillType objects
-	 * @author Isaac Pawling | 1805-WVU | Richard Orr
 	 */
 	@RequestMapping(method=RequestMethod.GET)
 	@ApiOperation(value = "Gets list of SkillTypes",
-		notes = "Enter Notes here",
 	    response = SkillType.class,
 	    responseContainer = "List")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "All SkillTypes returned") } )
@@ -53,14 +49,12 @@ public class SkillTypeController {
 	}
 	
 	/**
-	 * Request that returns all active skill types
+	 * Gets all active skill types
 	 * 
 	 * @return List of all active SkillType objects
-	 * @author Isaac Pawling | 1805-WVU | Richard Orr
 	 */
 	@RequestMapping(method=RequestMethod.GET, value = "/active")
 	@ApiOperation(value = "Gets list of active SkillTypes",
-		notes = "Enter Notes here",
 	    response = SkillType.class,
 	    responseContainer = "List")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "All active SkillTypes returned") } )
@@ -69,14 +63,12 @@ public class SkillTypeController {
 	}
 	
 	/**
-	 * Request that returns a skill type by id
+	 * Gets requested Id
 	 * 
 	 * @return SkillType object and Http status code 200 if found, 404 otherwise
-	 * @author Isaac Pawling | 1805-WVU | Richard Orr
 	 */
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	@ApiOperation(value = "Gets a SkillType by id",
-		notes = "Enter Notes here",
 	    response = SkillType.class)
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Requested SkillType returned"),
 			@ApiResponse(code = 404, message = "Requested SkillType not found") } )
@@ -90,14 +82,13 @@ public class SkillTypeController {
 	
 	
 	/**
-	 * Request that creates a skill type
+	 * Creates a skill type
 	 * 
-	 * @return SkillType w/ updated Id and Http status code 200, 
-	 * @author Isaac Pawling | 1805-WVU | Richard Orr
+	 * @return SkillType w/ updated Id and Http status code 200,
+	 * 	updating skilltype with empty title results in 406
 	 */
 	@RequestMapping(method=RequestMethod.POST)
 	@ApiOperation(value = "Creates a SkillType",
-		notes = "Enter Notes here",
 	    response = SkillType.class)
 	@ApiResponses(value = { @ApiResponse(code = 201, message = "SkillType created"),
 			@ApiResponse(code = 406, message = "SkillType must have a title") } )
@@ -109,14 +100,12 @@ public class SkillTypeController {
 	}
 	
 	/**
-	 * Request that puts skill type by id
+	 * Updates skilltype located at "/id"
 	 * 
-	 * @return Http status code 200
-	 * @author Isaac Pawling | 1805-WVU | Richard Orr
+	 * @return Http status code 202, 404 if skill type w/ provided Id DNE
 	 */
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	@ApiOperation(value = "Puts a SkillType by id",
-		notes = "Enter Notes here",
 	    response = SkillType.class)
 	@ApiResponses(value = { @ApiResponse(code = 202, message = "SkillType updated"),
 			@ApiResponse(code = 404, message = "SkillType not found") } )
@@ -129,14 +118,12 @@ public class SkillTypeController {
 	}
 	
 	/**
-	 * Request that deletes a skill
+	 * Deletes a skill
 	 * 
 	 * @return Http status code 404 if SkillType DNE, 204 otherwise
-	 * @author Isaac Pawling | 1805-WVU | Richard Orr
 	 */
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	@ApiOperation(value = "Deletes a SkillType by id",
-		notes = "Enter Notes here",
 	    response = SkillType.class)
 	@ApiResponses(value = { @ApiResponse(code = 204, message = "SkillType deleted"),
 			@ApiResponse(code = 404, message = "SkillType not found") } )
