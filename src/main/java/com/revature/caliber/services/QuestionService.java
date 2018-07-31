c24d722cfa6d815b4b76453d7c1934a2999a6816
Ethan Conner <EthanCo81@ymail.com>
2018-07-31 15:31:25 -0400

    Update QuestionService.java

--- src/main/java/com/revature/caliber/services/QuestionService.java
+++ src/main/java/com/revature/caliber/services/QuestionService.java
@@ -1,22 +1,25 @@
 package com.revature.caliber.services;
 
 import java.util.List;
+import io.swagger.annotations.ApiModel;
 
 import com.revature.caliber.beans.Question;
 
 /**
- * Interface that contains methods to be implemented for question status toggling and to grab
+ * Interface that contains associated with the Question POJO
  * 
  * @author Alexia Doramus | 1803-USF-MAR26 | Wezley Singleton
  * @author Joanel Vasquez | 1803-USF-MAR26 | Wezley Singleton
- *
+ * @author Isaac Pawling  | 1805-WVU | Richard Orr
+ * @author Ethan Conner	  | 1805-WVU -MAY29  | Richard Orr
+ * @author Adil Iqbal	  | 1805-WVU -MAY29  | Richard Orr
  */
 
+@ApiModel(value="QuestionService", description="Provides an interface for the Service layer")
 public interface QuestionService {
 	
 	
 	/**
-	 * @author Ethan Conner	  | 1805-WVU -MAY29  | Richard Orr
 	 * Add a question to the database
 	 * 
 	 * @param transient question
@@ -25,38 +28,39 @@ public interface QuestionService {
 	public Question create(Question question);
 	
 	/**
+	 * Gets all questions
+	 * 
+	 * @return Question List 
+	 */
+	public List<Question> getQuestions();
+	
+	/**
 	 * Gets questions based on bucketId
-	 * @author Ethan Conner	  | 1805-WVU -MAY29  | Richard Orr
 	 * @param bucketId of bucket
-	 * @return Question List
+	 * @return List of questions in the specified bucket
 	 */
 	public List<Question> getQuestionsByBucket(Integer bucketId);
 	
 	/**
 	 * Delete question by id
-	 * @author Adil Iqbal	  | 1805-WVU -MAY29  | Richard Orr
-	 * @param bucketId of bucket
-	 * @return Question List
+	 * @param questionId Id of question
+	 * No return
 	 */
-	
 	public void deleteByQuestionId(Integer questionId);
 	
 	/**
 	 * update question
-	 * @author Adil Iqbal	  | 1805-WVU -MAY29  | Richard Orr
-	 * @param bucketId of bucket
-	 * @return Question List
+	 * @param question - a Question object
+	 * @return updated Question object
 	 */
-	
 	public Question updateQuestion(Question question);
 	
 	/**
 	 * change the status of the question
-	 * @author Adil Iqbal	  | 1805-WVU -MAY29  | Richard Orr
-	 * @param bucketId of bucket
-	 * @return Question List
+	 * @param isActive - boolean determining if question is active
+	 * @param questionId - Id of question
+	 * No return
 	 */
-	
 	public void toggleQuestionStatus(Boolean isActive, Integer questionId);
-}
 
+}
