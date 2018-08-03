0be1c55a4bd9c1fbbb0d081acfe9fc836b6ce939
ethanco81 <ethanco81@ymail.com>
2018-08-03 10:48:56 -0400

    SQL and postman

---
+++ src/main/java/com/revature/caliber/beans/Weight.java
@@ -0,0 +1,141 @@
+package com.revature.caliber.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+
+
+/**
+ * 
+ * @author Ethan Conner | 1805-WVU-AUG3 | Richard Orr
+ * 
+ * POJO for the weight object
+ *
+ */
+@ApiModel(value = "Weight", description = "Weights for categories to determine how important a set of buckets"
+		+ "are during an interview")
+@Entity
+@Table(name = "weight")
+public class Weight {
+	
+	@ApiModelProperty(value = "The weightId - primary key for the table")
+	@Id
+	@Column(name= "weight_id")
+	private long weightId;
+	
+	@ApiModelProperty(value = "The actual weight needed for calculation")
+	@Column(name = "weight")
+	private int weight;
+	
+	@ApiModelProperty(value="the skillType Id")
+	@Column(name="skill_type_id")
+	private int skillTypeId;
+	
+	@ApiModelProperty(value="The category Id")
+	@Column(name = "category_id")
+	private int categoryId;
+
+	public Weight() {
+		super();
+	}
+
+	public Weight(int weightId, int weight, int skillTypeId, int categoryId) {
+		super();
+		this.weightId = weightId;
+		this.weight = weight;
+		this.skillTypeId = skillTypeId;
+		this.categoryId = categoryId;
+	}
+
+	/**
+	 * Getters and setters
+	 */
+	public long getWeightId() {
+		return weightId;
+	}
+
+	public void setWeightId(long weightId) {
+		this.weightId = weightId;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getSkillTypeId() {
+		return skillTypeId;
+	}
+
+	public void setSkillTypeId(int skillTypeId) {
+		this.skillTypeId = skillTypeId;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Weight [weightId=");
+		builder.append(weightId);
+		builder.append(", weight=");
+		builder.append(weight);
+		builder.append(", skillTypeId=");
+		builder.append(skillTypeId);
+		builder.append(", categoryId=");
+		builder.append(categoryId);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + categoryId;
+		result = prime * result + skillTypeId;
+		result = prime * result + weight;
+		result = prime * result + (int) (weightId ^ (weightId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Weight other = (Weight) obj;
+		if (categoryId != other.categoryId)
+			return false;
+		if (skillTypeId != other.skillTypeId)
+			return false;
+		if (weight != other.weight)
+			return false;
+		if (weightId != other.weightId)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,7 @@
-create table question(
-    question_id number(10) primary key,
-    bucket_id number (10) not null,
-    is_active number(1) not null,
-    question_text varchar2(500) not null,
-    sample_answer_1 varchar2(500) not null,
-    sample_answer_2 varchar2(500) not null,
-    sample_answer_3 varchar2(500) not null,
-    sample_answer_4 varchar2(500) not null,
-    sample_answer_5 varchar2(500) not null
-);
-
-create table bucket(
-    bucket_id number(10) primary key,
-    category_id number(10) not null,
-    bucket_description varchar2(100) not null,
-    is_active number(1) not null,
-    skill_type_id number(10) not null
-);
-
 create table skill_type(
     skill_type_id number(10) primary key,
-    title varchar2(100) not null
+    title varchar2(100) not null,
+    is_active number(1) not null
 );
 
 create table category(
@@ -29,11 +10,32 @@ create table category(
     is_active number(1) not null
 );
 
+create table bucket(
+    bucket_id number(10) primary key,
+    category_id number(10) not null,
+    bucket_description varchar2(100) not null,
+    is_active number(1) not null
+);
+
+create table question(
+    question_id number(10) primary key,
+    bucket_id number (10) not null, 
+    is_active number(1) not null,
+    question_text varchar2(600) not null,
+    sample_answer_1 varchar2(600) not null,
+    sample_answer_2 varchar2(600) not null,
+    sample_answer_3 varchar2(600) not null,
+    sample_answer_4 varchar2(600) not null,
+    sample_answer_5 varchar2(600) not null
+);
+
 create table weight(
     weight_id number(10) primary key,
     weight number(10) not null,
     category_id number(10) not null,
-    skill_type_id number(10) not null
+    skill_type_id number(10) not null,
+    foreign key (category_id) references category(category_id),
+    foreign key (skill_type_id) references skill_type(skill_type_id)
 );
 
 CREATE SEQUENCE IF NOT EXISTS QUESTION_SEQUENCE MINVALUE 100001 INCREMENT BY 1 START WITH 100001;
@@ -43,5 +45,3 @@ CREATE SEQUENCE IF NOT EXISTS BUCKET_SEQUENCE MINVALUE 100001 INCREMENT BY 1 STA
 CREATE SEQUENCE IF NOT EXISTS SKILL_TYPE_SEQUENCE MINVALUE 100001 INCREMENT BY 1 START WITH 100001;
 
 CREATE SEQUENCE IF NOT EXISTS CATEGORY_SEQUENCE MINVALUE  100001 INCREMENT BY 1 START WITH  100001;
-
-CREATE SEQUENCE IF NOT EXISTS WEIGHT_SEQUENCE MINVALUE  100001 INCREMENT BY 1 START WITH  100001;
