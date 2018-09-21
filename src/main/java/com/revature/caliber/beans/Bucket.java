d89b790f8f739f2e972e74c95336e59b246be765
Jeremy Straus <Jstraws@verizon.net>
2018-09-21 13:45:41 -0400

    Remove wrapper classes and clean up some methods

--- src/main/java/com/revature/caliber/beans/Bucket.java
+++ src/main/java/com/revature/caliber/beans/Bucket.java
@@ -1,26 +1,22 @@
 package com.revature.caliber.beans;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-
-
-import org.springframework.stereotype.Component;
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 
 /**
  * Define bucket POJO
- *  
+ *
  * @author Adil Iqbal | 1805-WVU-MAY29 | Richard Orr
+ * @author Jeremy Straus | 1807-QC | Emily Higgins
+ * @author Rishabh Rana | 1807-QC | Emily Higgins
+ * @author Alpha Barry | 1807-QC | Emily Higgins
+ * @author Omar Guzman | 1807-QC | Emily Higgins
  */
 @ApiModel(value = "Bucket", description = "A Bucket which holds a set of related Questions")
 @Component
@@ -28,103 +24,83 @@ import io.swagger.annotations.ApiModelProperty;
 @Table(name = "bucket")
 public class Bucket implements Serializable {
 
-	private static final long serialVersionUID = 2435095816452768808L;
-	
-	@ApiModelProperty(value = "id of the Bucket")
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BUCKET_SEQUENCE")
-	@SequenceGenerator(name = "BUCKET_SEQUENCE", sequenceName = "bucket_sequence", allocationSize=1)
-	@Column(name = "BUCKET_ID")
-	private Integer bucketId;
+    private static final long serialVersionUID = 2435095816452768808L;
 
-	@ApiModelProperty(value = "description of the Bucket")
-	@Column(name = "BUCKET_DESCRIPTION")
-	private String bucketDescription;
+    @ApiModelProperty(value = "id of the Bucket")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BUCKET_SEQUENCE")
+    @SequenceGenerator(name = "BUCKET_SEQUENCE", sequenceName = "bucket_sequence", allocationSize = 1)
+    @Column(name = "BUCKET_ID")
+    private int bucketId;
 
-	@ApiModelProperty(value = "denotes whether Bucket is active or not")
-	@Column(name = "IS_ACTIVE")
-	private Boolean isActive;
+    @ApiModelProperty(value = "description of the Bucket")
+    @Column(name = "BUCKET_DESCRIPTION")
+    private String bucketDescription;
 
-	public Bucket() {
-		super();
-	}	  
+    @ApiModelProperty(value = "denotes whether Bucket is active or not")
+    @Column(name = "IS_ACTIVE")
+    private boolean isActive;
 
-	public Bucket(Integer bucketId, String bucketDescription, Boolean isActive) {
-		super();
-		this.bucketId = bucketId;
-		this.bucketDescription = bucketDescription;
-		this.isActive = isActive;
-	}
-	
-	/**
-	 * Getters & Setters
-	 */
-	public Integer getBucketId() {
-		return bucketId;
-	}
+    public Bucket() {
+        super();
+    }
 
-	public void setBucketId(Integer bucketId) {
-		this.bucketId = bucketId;
-	}
+    public Bucket(Integer bucketId, String bucketDescription, Boolean isActive) {
+        super();
+        this.bucketId = bucketId;
+        this.bucketDescription = bucketDescription;
+        this.isActive = isActive;
+    }
 
-	public String getBucketDescription() {
-		return bucketDescription;
-	}
+    /**
+     * Getters & Setters
+     */
+    public int getBucketId() {
+        return bucketId;
+    }
 
-	public void setBucketDescription(String bucketDescription) {
-		this.bucketDescription = bucketDescription;
-	}
+    public void setBucketId(int bucketId) {
+        this.bucketId = bucketId;
+    }
 
-	public Boolean getIsActive() {
-		return isActive;
-	}
+    public String getBucketDescription() {
+        return bucketDescription;
+    }
 
-	public void setIsActive(Boolean isActive) {
-		this.isActive = isActive;
-	}
+    public void setBucketDescription(String bucketDescription) {
+        this.bucketDescription = bucketDescription;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((bucketDescription == null) ? 0 : bucketDescription.hashCode());
-		result = prime * result + ((bucketId == null) ? 0 : bucketId.hashCode());
-		result = prime * result + ((isActive == null) ? 0 : isActive.hashCode());
-		return result;
-	}
+    public boolean getIsActive() {
+        return isActive;
+    }
 
-	 @Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Bucket other = (Bucket) obj;
-		if (bucketDescription == null) {
-			if (other.bucketDescription != null)
-				return false;
-		} else if (!bucketDescription.equals(other.bucketDescription))
-			return false;
-		if (bucketId == null) {
-			if (other.bucketId != null)
-				return false;
-		} else if (!bucketId.equals(other.bucketId))
-			return false;
-		if (isActive == null) {
-			if (other.isActive != null)
-				return false;
-		} else if (!isActive.equals(other.isActive))
-			return false;
-		return true;
-	}
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
 
-	@Override
-	public String toString() {
-		return "Bucket [bucketId=" + bucketId + ", bucketDescription="
-				+ bucketDescription + ", isActive=" + isActive + "]";
-	}
-	
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bucket bucket = (Bucket) o;
+        return getBucketId() == bucket.getBucketId() &&
+                isActive == bucket.isActive &&
+                Objects.equals(getBucketDescription(), bucket.getBucketDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBucketId(), getBucketDescription(), isActive);
+    }
+
+    @Override
+    public String toString() {
+        return "Bucket{" +
+                "bucketId=" + bucketId +
+                ", bucketDescription='" + bucketDescription + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
 
