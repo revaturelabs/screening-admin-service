b04ecfb7919eab79df87bff6b4b603a783ba826e
Emily Higgins <emilyrchiggins@gmail.com>
2018-09-26 12:00:30 -0400

    updating to DiscoveryClient

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -2,10 +2,10 @@ package com.revature.caliber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableSwagger2
 
 @SpringBootApplication
