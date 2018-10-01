d18bcf75d918fcd743180455c1f79a1d1a16024a
Omar Guzman <omarguz225@gmail.com>
2018-10-01 11:08:36 -0400

    question controller

---
+++ src/test/java/com/revature/caliber/controllers/QuestionControllerTest.java
@@ -0,0 +1,44 @@
+package com.revature.caliber.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuestionControllerTest {
+
+	@Test
+	public void testGetQuestions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBucketQuestions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteByBucket() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteByQuestionId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testActivateQuestion() {
+		fail("Not yet implemented");
+	}
+
+}
