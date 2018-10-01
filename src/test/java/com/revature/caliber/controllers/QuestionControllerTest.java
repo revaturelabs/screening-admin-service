c267408e4137d4847edcb7fb8747343bd0910078
Omar Guzman <omarguz225@gmail.com>
2018-10-01 16:40:24 -0400

    QuestionControllerTest added

--- src/test/java/com/revature/caliber/controllers/QuestionControllerTest.java
+++ src/test/java/com/revature/caliber/controllers/QuestionControllerTest.java
@@ -1,44 +1,90 @@
 package com.revature.caliber.controllers;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.revature.caliber.Application;
+import static io.restassured.RestAssured.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.revature.caliber.beans.Bucket;
+import com.revature.caliber.beans.Question;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class QuestionControllerTest {
+	
+	private final String url="http://ec2-52-55-27-249.compute-1.amazonaws.com:8181";
 
 	@Test
 	public void testGetQuestions() {
-		fail("Not yet implemented");
+		given()
+		.when()
+			.get(url + "/question/all")
+		.then()
+			.statusCode(200);
 	}
 
 	@Test
 	public void testGetBucketQuestions() {
-		fail("Not yet implemented");
+		given()
+		.when()
+			.get(url + "/question/getByBucket/{bucketId}", 404)
+		.then()
+			.statusCode(200);
 	}
 
 	@Test
 	public void testDeleteByBucket() {
-		fail("Not yet implemented");
+		given()
+	       .when().delete(url + "/question/deleteByBucket/" + 404)
+	       .then().log().ifError()
+	       .assertThat().statusCode(200);
 	}
 
 	@Test
 	public void testCreate() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		Question question = new Question();
+		
+		given()
+			.contentType("application/json")
+			.body(question)
+		.when()
+			.post(url + "/new")
+		.then()
+			.statusCode(201);
 	}
 
 	@Test
 	public void testUpdateQuestion() {
-		fail("Not yet implemented");
+		Question question = new Question(1007, new Bucket(), false, "Test", "Test", "Test", "Test", "Test", "Test");
+		
+		given()
+			.contentType("application/json")
+			.body(question)
+		.when()
+			.put(url + "/question/update")
+		.then()
+			.statusCode(200);
 	}
 
 	@Test
 	public void testDeleteByQuestionId() {
-		fail("Not yet implemented");
+		int questionId = 1007;
+		given()
+	       .when().delete(url + "/question/delete/" + questionId)
+	       .then().log().ifError()
+	       .assertThat().statusCode(200);
 	}
 
 	@Test
 	public void testActivateQuestion() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		int questionId = 1007;
+		given()
+	       .when().put(url + "/question/toggle/" + questionId)
+	       .then().log().ifError()
+	       .assertThat().statusCode(204);
 	}
 
 }
