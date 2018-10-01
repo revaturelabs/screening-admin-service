e130a8c0c982579333f4743a5856a6a3e1522cad
AlphaBarr <41920229+AlphaBarr@users.noreply.github.com>
2018-10-01 10:55:32 -0400

    structure

---
+++ src/test/java/com/revature/caliber/controllers/BucketControllerTest.java
@@ -0,0 +1,34 @@
+package com.revature.caliber.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BucketControllerTest {
+
+	@Test
+	public void testGetAllBuckets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateBucket() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateBucket() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBucketByBucketId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteBucket() {
+		fail("Not yet implemented");
+	}
+
+}
