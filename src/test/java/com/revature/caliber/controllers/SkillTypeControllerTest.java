18c2358779f3796ef8a211370dc8b386ba6de90e
rrana22 <rishabhrana9592@gmail.com>
2018-10-01 20:18:25 -0400

    Added the test methods without implementation

--- src/test/java/com/revature/caliber/controllers/SkillTypeControllerTest.java
+++ src/test/java/com/revature/caliber/controllers/SkillTypeControllerTest.java
@@ -13,7 +13,36 @@ import com.revature.caliber.beans.Bucket;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-
 public class SkillTypeControllerTest {
+
+	@Test
+	public void testGetSkills() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActiveSkills() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSkillById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPostSkill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutSkillById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteSkillById() {
+		fail("Not yet implemented");
+	}
 
 }
