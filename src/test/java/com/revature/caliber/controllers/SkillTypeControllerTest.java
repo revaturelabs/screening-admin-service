025d1fe890c91b9cdc82e61fa9ba5d3a7952ed43
rrana22 <rishabhrana9592@gmail.com>
2018-10-01 20:12:56 -0400

    Created the test class for skill types

---
+++ src/test/java/com/revature/caliber/controllers/SkillTypeControllerTest.java
@@ -0,0 +1,19 @@
+package com.revature.caliber.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.caliber.Application;
+import com.revature.caliber.beans.Bucket;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class SkillTypeControllerTest {
+
+}
