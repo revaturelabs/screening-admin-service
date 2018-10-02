90f6ea4ebd68c805b3de5813fbe1bcbac3ba3522
Jeremy Straus <jstraws@verizon.net>
2018-10-02 16:02:57 -0400

    Add port and remove host.

--- src/test/java/com/revature/caliber/controllers/BucketControllerTest.java
+++ src/test/java/com/revature/caliber/controllers/BucketControllerTest.java
@@ -1,96 +1,97 @@
 package com.revature.caliber.controllers;
 
-import static org.junit.Assert.*;
-
+import com.revature.caliber.Application;
+import com.revature.caliber.beans.Bucket;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.revature.caliber.Application;
-import com.revature.caliber.beans.Bucket;
-
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class BucketControllerTest{
+public class BucketControllerTest {
 
 	@LocalServerPort
 	private int port;
-	private String host = "http://localhost:" + port;
-	
+
 	@Test
 	public void testGetConnectionToBucketEndpoint() {
 		given()
-		.port(port)
-		.when()
-			.get("/bucket")
-		.then()
-			.log()
-			.ifValidationFails()
-			.statusCode(200);
-			
+				.port(port)
+				.when()
+				.get("/bucket")
+				.then()
+				.log()
+				.ifValidationFails()
+				.statusCode(200);
+
 	}
 
 	@Test
 	public void testGetBucketByBucketId() {
 		given()
-		.when()
-			.get(host + "/bucket/{bucketId}", 404)
-		.then()
-			.body("bucketId", equalTo(404))
-			.statusCode(200);
+				.port(port)
+				.when()
+				.get("/bucket/{bucketId}", 404)
+				.then()
+				.body("bucketId", equalTo(404))
+				.statusCode(200);
 	}
-	
+
 	@Test
 	public void testCreateBucket() {
 		Bucket b = new Bucket(417, "Rest Assured Test Bucket", true);
-		
+
 		given()
-			.contentType("application/json")
-			.body(b)
-		.when()
-			.post(host + "/bucket")
-		.then()
-			.statusCode(201);
+				.port(port)
+				.contentType("application/json")
+				.body(b)
+				.when()
+				.post("/bucket")
+				.then()
+				.statusCode(201);
 	}
 
 	@Test
 	public void testUpdateBucket() {
 		Bucket b = new Bucket(1, "Updated Rest Assured Test", true);
-		
+
 		given()
-			.contentType("application/json")
-			.body(b)
-		.when()
-			.put(host + "/bucket/update")
-		.then()
-			.statusCode(200);
+				.port(port)
+				.contentType("application/json")
+				.body(b)
+				.when()
+				.put("/bucket/update")
+				.then()
+				.statusCode(200);
 	}
 
 	@Test
 	public void testDeleteBucket() {
 		given()
-		.when()
-			.delete(host + "/bucket/{bucketId}" , 404)
-		.then()
-			.statusCode(204);
+				.port(port)
+				.when()
+				.delete("/bucket/{bucketId}", 404)
+				.then()
+				.statusCode(204);
 	}
-	
-	
+
+
 	@Test
 	public void testCreateEmptyBucket() {
 		Bucket b = new Bucket();
 		given()
-			.body(b)
-		.when()
-			.post(host + "/bucket")
-		.then()
-			.statusCode(415);
+				.port(port)
+				.body(b)
+				.when()
+				.post("/bucket")
+				.then()
+				.statusCode(415);
 	}
-	
+
 
 }
