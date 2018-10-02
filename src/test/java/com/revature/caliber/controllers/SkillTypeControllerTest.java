2464ffd2e994db7a907f13a954bc66c030badd89
rrana22 <rishabhrana9592@gmail.com>
2018-10-01 21:02:41 -0400

    Finished tests for the SkillType Controller

--- src/test/java/com/revature/caliber/controllers/SkillTypeControllerTest.java
+++ src/test/java/com/revature/caliber/controllers/SkillTypeControllerTest.java
@@ -8,41 +8,113 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.revature.caliber.Application;
-import com.revature.caliber.beans.Bucket;
+import com.revature.caliber.beans.SkillType;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class SkillTypeControllerTest {
 
+	private String host = "http://ec2-52-55-27-249.compute-1.amazonaws.com:8181";
+	
 	@Test
 	public void testGetSkills() {
-		fail("Not yet implemented");
+		given()
+		.when()
+			.get(host + "/skilltype")
+		.then()
+			.statusCode(200);
 	}
 
 	@Test
 	public void testGetActiveSkills() {
-		fail("Not yet implemented");
+		given()
+		.when()
+			.get(host + "/skilltype/active")
+		.then()
+			.statusCode(200);
 	}
 
 	@Test
 	public void testGetSkillById() {
-		fail("Not yet implemented");
+		given()
+		.when()
+			.get(host + "/skilltype/{id}", 51)
+		.then()
+			.body("skillTypeId", equalTo(51))
+			.statusCode(200);
 	}
 
 	@Test
-	public void testPostSkill() {
-		fail("Not yet implemented");
+	public void testCreateSkill() {
+		SkillType st = new SkillType("Charisma", true);
+		
+		given()
+			.contentType("application/json")
+			.body(st)
+		.when()
+			.post(host + "/skilltype")
+		.then()
+			.statusCode(201);
+	}
+	
+	@Test
+	public void testCreateSkillWithoutTitle() {
+		SkillType st = new SkillType("", true);
+		
+		given()
+			.contentType("application/json")
+			.body(st)
+		.when()
+			.post(host + "/skilltype")
+		.then()
+			.statusCode(406);
 	}
 
 	@Test
-	public void testPutSkillById() {
-		fail("Not yet implemented");
+	public void testUpdateSkillById() {
+		SkillType st = new SkillType("Test Update", true);
+		
+		given()
+			.contentType("application/json")
+			.body(st)
+		.when()
+			.put(host + "/skilltype/{id}", 51)
+		.then()
+			.statusCode(202);
 	}
 
+	@Test
+	public void testUpdateSkillByIdForIdThatDoesNotExist() {
+		SkillType st = new SkillType("Test Update", true);
+		
+		given()
+			.contentType("application/json")
+			.body(st)
+		.when()
+			.put(host + "/skilltype/{id}", -1)
+		.then()
+			.statusCode(404);
+	}
+	
 	@Test
 	public void testDeleteSkillById() {
-		fail("Not yet implemented");
+		given()
+		.when()
+			.delete(host + "/skilltype/{id}" , 56)
+		.then()
+			.statusCode(204);
+	}
+	
+	@Test
+	public void testDeleteSkillByIdForIdThatDoesNotExist() {
+		given()
+		.when()
+			.delete(host + "/skilltype/{id}" , -1)
+		.then()
+			.statusCode(404);
 	}
 
 }
