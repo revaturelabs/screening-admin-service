6ecdf7ea8f88d7d83fa3def87667b93ab6861b0e
Jeremy Straus <jstraws@verizon.net>
2018-10-03 18:27:48 -0400

    Use Zuul!

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -3,11 +3,12 @@ package com.revature.caliber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @EnableSwagger2
-
+@EnableZuulProxy
 @SpringBootApplication
 public class Application {
 
