843d953e8958649cedfc258868c5d7431a4260d1
Jeremy Straus <jstraws@verizon.net>
2018-10-03 10:06:37 -0400

    Update to use localhost

--- src/test/java/com/revature/caliber/controllers/QuestionControllerTest.java
+++ src/test/java/com/revature/caliber/controllers/QuestionControllerTest.java
@@ -1,92 +1,103 @@
 package com.revature.caliber.controllers;
 
+import com.revature.caliber.Application;
+import com.revature.caliber.beans.Question;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.revature.caliber.Application;
-import static io.restassured.RestAssured.*;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.revature.caliber.beans.Bucket;
-import com.revature.caliber.beans.Question;
+
+import static io.restassured.RestAssured.given;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class QuestionControllerTest {
-	
-	private final String url="http://ec2-52-55-27-249.compute-1.amazonaws.com:8181";
+
+	@LocalServerPort
+	int port;
 
 	@Test
 	public void testGetQuestions() {
 		given()
-		.when()
-			.get(url + "/question/all")
-		.then()
-			.statusCode(200);
+				.port(port)
+				.when()
+				.get("/question/all")
+				.then()
+				.statusCode(200);
 	}
 
 	@Test
 	public void testGetBucketQuestions() {
 		given()
-		.when()
-			.get(url + "/question/getByBucket/{bucketId}", 404)
-		.then()
-			.statusCode(200);
+				.port(port)
+				.when()
+				.get("/question/getByBucket/{bucketId}", 404)
+				.then()
+				.statusCode(200);
 	}
 
 	@Test
 	public void testDeleteByBucket() {
 		given()
-	       .when().delete(url + "/question/deleteByBucket/" + 404)
-	       .then().log().ifError()
-	       .assertThat().statusCode(204);
+				.port(port)
+				.when().delete("/question/deleteByBucket/" + 404)
+				.then().log().ifError()
+				.assertThat().statusCode(204);
 	}
 
 	@Test
 	public void testCreate() {
-		//fail("Not yet implemented");
-		Question question = new Question(99999, new Bucket(), false, "Test", "Test", "Test", "Test", "Test", "Test");
-		
+		Question question = new Question(99999, null, false, "Test", "Test", "Test", "Test", "Test", "Test");
+
 		given()
-			.contentType("application/json")
-			.body(question)
-		.when()
-			.post(url + "/question/new")
-		.then()
-			.statusCode(201);
+				.port(port)
+				.contentType("application/json")
+				.body(question)
+				.when()
+				.post("/question/new")
+				.then()
+				.statusCode(201);
 	}
 
 	@Test
 	public void testUpdateQuestion() {
-		Question question = new Question(1007, new Bucket(), false, "Test", "Test", "Test", "Test", "Test", "Test");
-		
+		Question question = new Question(1007, null, false, "Test", "Test", "Test", "Test", "Test", "Test");
+
 		given()
-			.contentType("application/json")
-			.body(question)
-		.when()
-			.put(url + "/question/update")
-		.then()
-			.statusCode(200);
+				.port(port)
+				.contentType("application/json")
+				.body(question)
+				.when()
+				.put("/question/update")
+				.then()
+				.statusCode(200);
 	}
 
 	@Test
 	public void testDeleteByQuestionId() {
 		int questionId = 1007;
-		
+
 		given()
-		.when()
-			.delete(url + "/question/deleteByBucket/{bucketId}" , questionId)
-		.then()
-			.statusCode(204);
+				.port(port)
+				.when()
+				.delete("/question/deleteByBucket/{bucketId}", questionId)
+				.then()
+				.statusCode(204);
 	}
 
 	@Test
 	public void testActivateQuestion() {
-		//fail("Not yet implemented");
 		int questionId = 1007;
-		given()
-	       .when().put(url + "/question/toggle/" + questionId)
-	       .then().log().ifError()
-	       .assertThat().statusCode(204);
-	}
 
+		given()
+				.port(port)
+				.when()
+				.put("/question/toggle/" + questionId)
+				.then()
+				.log()
+				.ifError()
+				.assertThat()
+				.statusCode(204);
+	}
 }
