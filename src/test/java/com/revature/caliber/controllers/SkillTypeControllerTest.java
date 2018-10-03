90c9b6ca8620fee446b74ed3a35765f0da665b42
Jeremy Straus <jstraws@verizon.net>
2018-10-03 13:36:58 -0400

    Add import.

--- src/test/java/com/revature/caliber/controllers/SkillTypeControllerTest.java
+++ src/test/java/com/revature/caliber/controllers/SkillTypeControllerTest.java
@@ -6,6 +6,7 @@ import com.revature.caliber.services.SkillTypeServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
