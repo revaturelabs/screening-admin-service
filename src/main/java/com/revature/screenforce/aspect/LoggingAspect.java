ba8f1b59a07e7cc8d1ef1c6603bb221efcd4c7c5
TuckerBelton <tuckerbelton@yahoo.com>
2019-05-24 09:45:55 -0400

    Services and Gateway up and running

--- src/main/java/com/revature/screenforce/aspect/LoggingAspect.java
+++ src/main/java/com/revature/screenforce/aspect/LoggingAspect.java
@@ -2,22 +2,22 @@ package com.revature.screenforce.aspect;
 
 import java.util.Arrays;
 
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-/*
+/**
  * Provides an aspect to modularize the logging concern for the service.
  * @author Isaac Pawling | 1805-WVU | Richard Orr
  */
 @Component
 @Aspect
 public class LoggingAspect {
-    /*
+    /**
      * Applies advice to join points matched by everything()
      * Logs the method called and the parameters passed.
      * If any errors are thrown, the stacktrace is logged.
@@ -27,30 +27,42 @@ public class LoggingAspect {
      * @return obj the object returned by the function
      * @author Isaac Pawling | 1805-WVU | Richard Orr
      */
-    @Around("everything()")
-    public Object log(ProceedingJoinPoint pjp) throws Throwable {
-        Object obj = null;
-        Logger log = LogManager.getLogger(pjp.getTarget().getClass());
-        log.info("Method with signature: "+pjp.getSignature());
-        log.info("With arguments: "+Arrays.toString(pjp.getArgs()));
-     
-        try {
-            obj = pjp.proceed();
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            for(StackTraceElement s : e.getStackTrace()) {
-                log.warn(s);
-            }
-        }
-        log.info(pjp.getSignature()+" returned: "+obj);
-        return obj;
-    }   
-    
-    /*
+//    @Around("everything()")
+//    public Object log(ProceedingJoinPoint pjp) throws Throwable {
+//        Object obj = null;
+//        Logger log = LogManager.getLogger(pjp.getTarget().getClass());
+//        log.info("Method with signature: "+pjp.getSignature());
+//        log.info("With arguments: "+Arrays.toString(pjp.getArgs()));
+//     
+//        try {
+//            obj = pjp.proceed();
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//            for(StackTraceElement s : e.getStackTrace()) {
+//                log.warn(s);
+//            }
+//        }
+//        log.info(pjp.getSignature()+" returned: "+obj);
+//        return obj;
+//    }
+//    
+//	@AfterThrowing(pointcut = "within(*)", throwing = "t")
+//    public void logAfterThrow(JoinPoint jp, Throwable t) {
+//        Logger log = LogManager.getLogger(jp.getTarget().getClass());
+//        log.error(jp.getSignature() + " threw " + t.getClass() + " with stack trace " + t.getStackTrace());
+//    }
+//    @AfterReturning(pointcut = "within(com.revature.screenforce.*)")
+//    public void logAfter(JoinPoint jp) {
+//        Logger log = LogManager.getLogger(jp.getTarget().getClass());
+//        log.info("Method with signature: "+jp.getSignature());
+//       log.info("With arguments: "+Arrays.toString(jp.getArgs()));
+//    }
+//    
+    /**
      * Specifies the predicate to match join points.
      * All spring bean methods in the application are matched. 
      * @author Isaac Pawling | 1805-WVU | Richard Orr
      */
-    @Pointcut("execution(* com.revature.screenforce..*(..))")
-    public void everything() { /* Empty method for Aspect Pointcut */ }
+//    @Pointcut("execution(* com.revature.screenforce..*(..))")
+//    public void everything() { /* Empty method for Aspect Pointcut */ }
 }
