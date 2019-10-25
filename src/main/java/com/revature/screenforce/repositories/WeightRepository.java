06241b86171385c02bc0894e9595923b4c1b18c0
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-24 20:03:28 -0400

    changed SkillType to Track

--- src/main/java/com/revature/screenforce/repositories/WeightRepository.java
+++ src/main/java/com/revature/screenforce/repositories/WeightRepository.java
@@ -17,26 +17,26 @@ public interface WeightRepository extends JpaRepository<Weight, Integer> {
     /**
      * Delete weights by skill type ID
      *
-     * @param skillTypeId Skill type ID
+     * @param trackId Skill type ID
      */
-    void deleteAllBySkillTypeSkillTypeId(int skillTypeId);
+    void deleteAllByTrackTrackId(int trackId);
 
     /**
      * Get all weights by skill type ID
      *
-     * @param skillTypeId Skill type ID
+     * @param trackId Skill type ID
      * @return List of weights
      */
-    List<Weight> getAllBySkillTypeSkillTypeId(int skillTypeId);
+    List<Weight> getAllByTrackTrackId(int trackId);
 
     /**
      * Get weights by skill type ID and category ID
      *
-     * @param skillTypeId Skill type ID
+     * @param trackId Skill type ID
      * @param categoryId Category ID
      * @return Weight
      */
-    Weight getBySkillTypeSkillTypeIdAndCategoryCategoryId(int skillTypeId, int categoryId);
+    Weight getByTrackTrackIdAndCategoryCategoryId(int trackId, int categoryId);
 
     /**
      * Delete weights by category ID
